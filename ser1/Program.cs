fac7074fbcca7abf53399eb4196194defdd5e5d5
student <student@BRAVO15.ms.polsl.local>
2019-12-09 16:43:12 +0100

    Finished XML serialization

---
+++ ser1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ser1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<User> users = new List<User>();
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Insert Client's full name: ");
+                string n = Console.ReadLine();
+                Console.WriteLine("Insert Client's address: ");
+                string a = Console.ReadLine();
+                User u = new User();
+                u.Name = n;
+                u.Address = a;
+                users.Add(u);
+
+            }
+
+            foreach (User u in users)
+            {
+                Console.WriteLine(u.ID + ", " + u.Name + ", " + u.Address);
+                u.WriteXML("userlist");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ser1/User.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace ser1
+{
+
+    public class User
+    {
+        
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+
+        private string address;
+
+        public string Address
+        {
+            get { return this.address; }
+            set { this.address = value; }
+        }
+
+        private static List<bool> UsedCounter = new List<bool>();
+        private static object Lock = new object();
+        private int id;
+        public int ID { get { return this.id; } }
+
+        private int GetAvailableIndex()
+        {
+            for (int i = 0; i < UsedCounter.Count; i++)
+            {
+                if (UsedCounter[i] == false)
+                {
+                    return i;
+                }
+            }
+
+            // Nothing available.
+            return -1;
+        }
+
+        public User()
+        {
+            lock (Lock)
+            {
+                int nextIndex = GetAvailableIndex();
+                if (nextIndex == -1)
+                {
+                    nextIndex = UsedCounter.Count;
+                    UsedCounter.Add(true);
+                }
+
+                this.id = nextIndex;
+
+
+            }
+
+        }
+        public void Dispose()
+        {
+            lock (Lock)
+            {
+                UsedCounter[ID] = false;
+            }
+        }
+
+        public void WriteXML(string path)
+        {
+
+            XmlSerializer writer =
+                new System.Xml.Serialization.XmlSerializer(this.GetType());
+
+
+            try
+            {
+                string file = path + ID.ToString() + ".xml";
+
+                //FileStream file = System.IO.File.Create(path);
+                StreamWriter sw = File.CreateText(file);
+                writer.Serialize(sw, this);
+                sw.Close();
+            }
+            catch (IOException e) { Console.WriteLine("IOException" + e); }
+            catch (Exception e) { Console.WriteLine("Exception" + e); }
+
+
+        }
+    }
+}
